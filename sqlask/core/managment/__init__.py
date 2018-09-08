464d25db7ccad47a7238295077307db4d56ec25a
venkata.katepalli <venkata.katepalli@inmar.com>
2018-09-08 17:37:50 +0530

    find commands based on the folder and verify it

--- sqlask/core/managment/__init__.py
+++ sqlask/core/managment/__init__.py
@@ -1,18 +1,48 @@
 import sqlask
 import sys
 import os
+import pkgutil
 
 class Managment(object):
 
 	def __init__(self, argv=None):
 		self.argv = argv or sys.argv[:]
-		self.command_arg0 = os.path.basename(self.argv[0])
-		self.command_arg1 = os.path.basename(self.argv[1]) or None
+
+
+	def get_all_commands(self):
+	    """
+	    Given a path to a management directory, returns a list of all the command
+	    names that are available.
+
+	    Returns an empty list if no commands are defined.
+	    """
+	    command_dir = os.path.join(__path__[0], 'commands')
+	    commands = [name for ins, name, is_pkg in pkgutil.iter_modules([command_dir])
+	            if not is_pkg and not name.startswith('_')]
+	    return commands
+
+
+	def check_command(self, command):
+		all_commands = self.get_all_commands()
+		print(all_commands)
+		if command in all_commands:
+			print(command+" Processing. Please wait...")
+		else:
+			print('Invalid Command...:(')
+
 
 	def process(self):
-		print("You Entered")
-		print(self.command_arg0)
-		print(self.command_arg1)
+		# get the required operation command
+		try:
+			command = self.argv[1]
+		except Exception as e:
+			command = 'help'
+		# if any operation requied or else help
+		if command == 'help':
+			print("Use some operations.")
+		else:
+			self.check_command(command)
+
 
 def main(argv=None):
     print("Executing command")
