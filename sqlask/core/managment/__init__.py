d5957e5a03f32a09c84681c447ea917d41db245f
venkata.katepalli <venkata.katepalli@inmar.com>
2018-09-08 15:54:26 +0530

    added basic command usage

--- sqlask/core/managment/__init__.py
+++ sqlask/core/managment/__init__.py
@@ -1,4 +1,20 @@
 import sqlask
+import sys
+import os
 
-def main():
-    print("Test Done")
+class Managment(object):
+
+	def __init__(self, argv=None):
+		self.argv = argv or sys.argv[:]
+		self.command_arg0 = os.path.basename(self.argv[0])
+		self.command_arg1 = os.path.basename(self.argv[1]) or None
+
+	def process(self):
+		print("You Entered")
+		print(self.command_arg0)
+		print(self.command_arg1)
+
+def main(argv=None):
+    print("Executing command")
+    management = Managment(argv)
+    management.process()
